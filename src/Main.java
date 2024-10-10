3a3ceb100f2653f6becda3bf7f0e7be067b5ce34
Anas Ali <mynaameis321@gmail.com>
2024-10-10 09:47:12 +0500

    member 1 task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
+    public int add(int a , int b){
+        return a+b;
+    }
+   public int multiplication(int a ,int b){
+        return a*b;
+   }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Main m= new Main();
+        int sum=0;
+        System.out.println("sum of two nums");
+        sum=m.add(3,4);
+        System.out.println(sum);
+        int mul=0;
+        System.out.println("\n multiplication of two nums");
+        mul=m.multiplication(4,8);
+        System.out.println(mul);
     }
 }
